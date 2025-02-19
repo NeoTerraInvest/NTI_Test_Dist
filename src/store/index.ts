d99d77b833835020422aeb6527e963c92953717a
4Bee <4bee.code@gmail.com>
2025-02-19 16:12:25 +0900

    🔥: reset main branch

--- src/store/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as store } from './store';
-export type { RootState, AppDispatch } from './store';
-export { default as languageSlice, eng, kor } from './function/languageSlice';
--- wasm-lib/src/lib.rs
+++
@@ -1,9 +0,0 @@
-use wasm_bindgen::prelude::*;
-
-mod language_manager;
-pub use crate::language_manager::language_manager;
-
-#[wasm_bindgen]
-pub fn greet(name: &str) -> String {
-    format!("Hello, {}! This is Rust and WebAssembly.", name)
-}
