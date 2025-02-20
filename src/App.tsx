e25801bad76590bebda753bf7cbad22ee2fc3e71
4Bee <4bee.code@gmail.com>
2025-02-20 19:58:54 +0900

    feat: test query parameter

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Test from "./components/dev/Test";
 import Home from "./page/Home";
 import Error from "./utils/Error";
+import { useEffect } from "react";
 
-const testQeury = import.meta.env.VITE_TEST_QUERY;
+const testQuery = import.meta.env.VITE_TEST_QUERY;
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (testQuery) {
+      navigate(`/test?${testQuery}`); // ✅ .env 값으로 자동 이동
+    }
+  }, []);
   return (
     <Routes>
       <Route index path="/" element={<Home />} />
-      <Route path={`/test?${testQeury}`} element={<Test />} />
+      <Route path="/test" element={<Test />} />
       <Route path="*" element={<Error statusCode={404} />} />
     </Routes>
   );
